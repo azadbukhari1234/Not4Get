0bda2eab63b08fb8134a39e056cf81dc510bd8a2
azadbukhari1234 <azadbukhariaziz@outlook.com>
2025-06-04 19:25:47 +0800

    up

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-  $('#actionBtn').on('click', function() {
-      $('#actionMsg').fadeIn(200).delay(1200).fadeOut(400);
-  });
-    $('#menuBtn').on('click', function() {
-      alert('Menu button clicked! (Add your menu logic here)');
-    });
+import { createapp } from 'vue'
+import app from './app.vue'
 
-});
